5b14729b93bc19b6165e279ea897979df3dc72b6
Toad <givitera@gmail.com>
2026-09-20 22:08:54 +0300

    test(parse_ingredient): fix test data for parse_only_name

--- RecipeSite/tests/test_parse_ingredient.py
+++ RecipeSite/tests/test_parse_ingredient.py
@@ -166,7 +166,7 @@ class ParseIngredientTest(TestCase):
 
     def test_parse_only_name(self):
         data = [
-            ['перец',
+            ['перецц',
              {'name': 'перец',
               'amount': "",
               'unit': "",
